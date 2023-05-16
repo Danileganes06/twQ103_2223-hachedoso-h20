026f756bb82fb11a407a810e61bf54ce6b8d40ab
Danileganes06 <80398702+Danileganes06@users.noreply.github.com>
2023-05-16 19:20:57 +0200

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -1,1 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+  
+  int main(){
+  int alternativa;
+  char opcion;
+  
+  do{
 
+
+
+
+    printf("Elija la opci%cn que desee\n",162);
+    printf("A-Registrar Datos\n",162);
+    printf("B-Analizar datos\n");
+    printf("C-Visualizar Datos\n");
+    printf("D-Salir\n");
+
+
+    scanf("%c",&opcion);
+    fflush(stdin);
+
+   switch(opcion){
+   	case 'A':
+   	case 'a':
+   	    break;
+   	
+   	case 'B':
+   	case 'b':
+   	    break;
+   	
+   	case 'C':
+   	case'c':
+   	    break;
+   	
+   	case 'D':
+   	case'd':
+   		return 0;
+   	    break;
+   	
+   	default:
+        printf("\nNo has elegido ninguna de las opciones");
+        break;
+   }
+
+
+}
+
+while(alternativa==1);
+
+    return 0;
+}
